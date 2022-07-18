f0ed38939324508ad52fb2b1c645868b835110b0
TudorAnitas <anitas_tudor@yahoo.com>
2022-07-18 10:51:03 +0300

    Add TextInputLayout around the fields in the register page Show error when the "password" and "repeat password" fields don't match

--- app/src/main/java/com/example/internshipui_hw1/RegisterActivity.kt
+++ app/src/main/java/com/example/internshipui_hw1/RegisterActivity.kt
@@ -3,15 +3,30 @@ package com.example.internshipui_hw1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
+
+    private fun passwordsMismatch(password : EditText, repeatPassword : EditText) : Boolean{
+        return if(password.text.toString() != repeatPassword.text.toString()){
+            repeatPassword.error = "Passwords don't match"
+            true
+        } else {
+            false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         val toolbar : androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbarRegister)
+        val passwordEditText : EditText = findViewById(R.id.passwordRegisterEditText)
+        val passwordRepeatEditText : EditText = findViewById(R.id.passwordRepeatRegisterEditText)
         val registerButton : Button = findViewById(R.id.registerButton)
         val goToLoginText : TextView = findViewById(R.id.goToLoginText)
 
@@ -22,8 +37,10 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         registerButton.setOnClickListener{
-            val mainIntent : Intent = Intent(this, MainActivity::class.java)
-            startActivity(mainIntent)
+            if(!passwordsMismatch(passwordEditText, passwordRepeatEditText)) {
+                val mainIntent: Intent = Intent(this, MainActivity::class.java)
+                startActivity(mainIntent)
+            }
         }
 
         goToLoginText.setOnClickListener{
@@ -31,6 +48,14 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(loginIntent)
         }
 
+        passwordRepeatEditText.setOnEditorActionListener{_, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE){
+                passwordsMismatch(passwordEditText, passwordRepeatEditText)
+            }
+             false
+        }
+
 
     }
+
 }
