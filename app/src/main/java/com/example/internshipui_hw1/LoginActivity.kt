f6ce4e198584c9a542a66e5e583113e3588035e1
TudorAnitas <anitas_tudor@yahoo.com>
2022-07-18 12:07:23 +0300

    Improve the UX, implementing a few UI and logic modifications
    - add states to the login button (active/inactive) depending on the username and password fields
    
    - add corner radius to fields, both in login page and register
    
    - change the text color from the bottom of login and register pages to mild_blue
    
    - user cannot access MainActivity if the fields in the login page are incomplete

--- app/src/main/java/com/example/internshipui_hw1/LoginActivity.kt
+++ app/src/main/java/com/example/internshipui_hw1/LoginActivity.kt
@@ -3,17 +3,48 @@ package com.example.internshipui_hw1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 
 class LoginActivity : AppCompatActivity() {
+
+    private fun fieldAreEmpty(username : EditText, password : EditText) : Boolean {
+        return username.text.toString().isEmpty() || password.text.toString().isEmpty()
+    }
+
+    private fun enableButton(username: EditText, password: EditText, button : Button){
+        if(!fieldAreEmpty(username, password)){
+            button.isEnabled = true
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         val toolbar : androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbarLogin)
+        val loginButton : Button = findViewById(R.id.loginButton)
+        val goToRegisterText : TextView = findViewById(R.id.goToRegisterText)
+        val usernameEditText : EditText = findViewById(R.id.usernameLoginEditText)
+        val passwordEditText : EditText = findViewById(R.id.passwordLoginEditText)
+
+        passwordEditText.addTextChangedListener{
+            loginButton.isEnabled =
+                usernameEditText.text.toString().isNotEmpty() && passwordEditText.text.toString().isNotEmpty()
+        }
+
         toolbar.title = resources.getString(R.string.loginActivity);
 
-        val goToRegisterText : TextView = findViewById(R.id.goToRegisterText)
+        loginButton.setOnClickListener{
+            if(!fieldAreEmpty(usernameEditText, passwordEditText)) {
+                val mainActivityIntent: Intent = Intent(this, MainActivity::class.java)
+                startActivity(mainActivityIntent)
+            }
+        }
+
         goToRegisterText.setOnClickListener{
             val registerIntent : Intent = Intent(this, RegisterActivity::class.java)
             startActivity(registerIntent)
